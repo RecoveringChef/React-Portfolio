931f316400af668c755c41e531da20763070d2a3
RecoveringChef <54086786+RecoveringChef@users.noreply.github.com>
2019-12-04 13:02:22 -0500

    header and footer in place, colors picked. playing with spacing

---
+++ src/components/header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <header className="header">
+
+      <h1>Dan Crenshaw</h1>
+
+      <p>The fullstack developer from FL.  No relation to the Congressman from TX.</p>
+    </header>
+  );
+}
+
+export default Header;
