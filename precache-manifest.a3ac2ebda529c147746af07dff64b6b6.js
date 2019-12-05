6292252484b734d191987e354fd51633219b7fc9
RecoveringChef <54086786+RecoveringChef@users.noreply.github.com>
2019-12-05 01:26:14 -0500

    Updates

---
+++ precache-manifest.a3ac2ebda529c147746af07dff64b6b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "299f85e1e91fe0bd2a52c0e68bbfe1d1",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "92916a4167e22f9cc9b4",
+    "url": "/React-Portfolio/static/css/main.4a668dc4.chunk.css"
+  },
+  {
+    "revision": "5a00491b12b47ea56197",
+    "url": "/React-Portfolio/static/js/2.1d0b6ff7.chunk.js"
+  },
+  {
+    "revision": "92916a4167e22f9cc9b4",
+    "url": "/React-Portfolio/static/js/main.dd72c1d9.chunk.js"
+  },
+  {
+    "revision": "7ddf653753334d186d39",
+    "url": "/React-Portfolio/static/js/runtime-main.b94f0b60.js"
+  }
+]);
